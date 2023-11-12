a645d5ac63340ff67b44ecfa99d9d4b69645329d
Brian Whisler <brian.whisler@gmail.com>
2023-11-12 10:58:02 -0700

    combine home and about me pages into one

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import Error from './pages/Error';
+import Loading from './components/Loading.jsx';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import About from './pages/About';
 import Work from './pages/Work';
 
 // Define the accessible routes, and which components respond to which URL
@@ -21,24 +21,19 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
-        title:'Home'
-      },
-      {
-        path: '/About',
-        element: <About />,
       },
       {
         path: '/Work',
-        element: <Work />,
+        element: <Work />
       },
       {
         path: '/Contact',
-        element: <Contact />,
-      },
-    ],
-  },
+        element: <Contact />
+      }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} fallbackElement={<Loading />} />
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    document.title = "Home";  
+    document.title = 'Home';
   }, []);
 
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1>About Me</h1>
       <p>
-        Welcome to my Portfolio! My name is Brian Whisler, and I'm a parent of
+        Welcome to my Portfolio! My name is Brian Whisler, and I&apos;m a parent of
         two kids who bring boundless levels of entertainment to my life, and
-        teach me about the world around me. When I'm not spending time with my
+        teach me about the world around me. When I&apos;m not spending time with my
         family, I enjoy exploring the great outdoors through hiking, biking, and
-        running, building LEGO sets and coding. There's nothing like being
+        running, building LEGO sets and coding. There&apos;s nothing like being
         surrounded by nature and pushing my physical limits to clear my mind and
         recharge my body.
+      </p>
+      <p>
+        In addition to my passion for outdoor activities, I am also an
+        experienced network technician, photographer and web designer. Over the
+        years, I&apos;ve honed my skills and expertise to provide top-notch services
+        to my clients, always striving to exceed expectations and deliver real
+        value. I&apos;m committed to continuous learning and growth, to stay
+        up-to-date with the latest technology in my field.
+      </p>
+      <p>
+        Thank you for visiting my Portfolio. Please feel free to contact me if
+        you have any questions or would like to learn more about my work and
+        services.
       </p>
     </div>
   );
