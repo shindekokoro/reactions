3fa4d214a3ae4da539861ad8491fa04f13308ed7
Brian Whisler <brian.whisler@gmail.com>
2023-11-13 19:52:06 -0700

    one index.js for all ./pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Code from './Code';
+import Contact from './Contact';
+import Error from './Error';
+import Home from './Home';
+import Photo from './Photo';
+
+export { Code, Contact, Error, Home, Photo };
