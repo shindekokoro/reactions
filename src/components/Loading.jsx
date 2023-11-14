7f99394ae6eb27bd1e2cddcd1cf24335bffebb6d
Brian Whisler <brian.whisler@gmail.com>
2023-11-13 20:03:40 -0700

    potential loading animation

---
+++ src/components/Loading.jsx
@@ -0,0 +1,22 @@
+import './Loading.css'
+
+export default function About() {
+  return (
+    <div id="loading">
+      <ul>
+        <li>Loading.</li>
+        <li>Loading..</li>
+        <li>Loading...</li>
+        <li>Loading....</li>
+        <li>Loading.....</li>
+        <li>Loading......</li>
+        <li>Loading.</li>
+        <li>Loading..</li>
+        <li>Loading...</li>
+        <li>Loading....</li>
+        <li>Loading.....</li>
+        <li>Loading......</li>
+      </ul>
+    </div>
+  );
+}
