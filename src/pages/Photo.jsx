c6f17214884a021f87daa789340c377c01b2f88c
Brian Whisler <brian.whisler@gmail.com>
2023-11-13 19:50:10 -0700

    Rename/split work.jsx to code.jsx/photo.jsx

---
+++ src/pages/Photo.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+export default function Work() {
+  useEffect(() => {
+    document.title = 'Photography Projects';
+  }, []);
+
+  return (
+    <ul className="work-container">
+      <li className="zack-and-hillary">
+        <a href="https://sirbrianphotography.wordpress.com/2009/10/26/zack-and-hillary/">
+          <p>
+            Zack & Hillary&apos;s Wedding <span>photography</span>
+          </p>
+        </a>
+      </li>
+      <li className="hillarys-graduation">
+        <a href="https://sirbrianphotography.wordpress.com/2010/04/08/a-graduation/">
+          <p> 
+            Hillary&apos;s Graduation <span>photography</span>
+          </p>
+        </a>
+      </li>
+    </ul>
+  );
+}
