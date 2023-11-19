c89961ff84b173cda9d1019ed16049e75f9cff96
Brian Whisler <brian.whisler@gmail.com>
2023-11-18 20:43:13 -0700

    stylize home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import { Typography } from '@mui/material';
 
 export default function Home() {
+  let paragraphSX = {
+    '&::first-letter': {
+      textTransform: 'capitalize',
+      fontWeight: 'bold'
+    }
+  };
+
   return (
     <div>
       <img
@@ -8,8 +15,9 @@ export default function Home() {
         alt="Profile Photo of Brian Whisler"
         width={200}
         loading="true"
+        style={{ float: 'left', paddingRight: '20px', borderRadius: '50%' }}
       />
-      <Typography>
+      <Typography variant="h6" pb={'15px'} sx={paragraphSX}>
         Welcome to my Portfolio! My name is Brian Whisler, and I&apos;m a parent
         of two kids who bring boundless levels of entertainment to my life, and
         teach me about the world around me. When I&apos;m not spending time with
@@ -18,7 +26,7 @@ export default function Home() {
         being surrounded by nature and pushing my physical limits to clear my
         mind and recharge my body.
       </Typography>
-      <Typography>
+      <Typography variant="h6" pb={'15px'} sx={paragraphSX}>
         In addition to my passion for outdoor activities, I am also an
         experienced network technician, photographer and web designer. Over the
         years, I&apos;ve honed my skills and expertise to provide top-notch
@@ -26,7 +34,7 @@ export default function Home() {
         deliver real value. I&apos;m committed to continuous learning and
         growth, to stay up-to-date with the latest technology in my field.
       </Typography>
-      <Typography>
+      <Typography variant="h6" pb={'15px'} sx={paragraphSX}>
         Thank you for visiting my Portfolio. Please feel free to contact me if
         you have any questions or would like to learn more about my work and
         services.
