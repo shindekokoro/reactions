0da934902b47c95e2acd197e97e4f222e6e5bff5
Brian Whisler <brian.whisler@gmail.com>
2023-11-18 22:31:46 -0700

    remove rollup options

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,
-    open: true,
-  },
-  build: {
-    rollupOptions: {
-        external: [
-            "react", // ignore react stuff
-            "react-dom",
-            "react-icons/gr",
-            "react-icons/di"
-        ],
-    }
-},
+    open: true
+  }
 });
