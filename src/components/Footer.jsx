becf9d1e7b20dec59a7f2a0664c749f85c3da646
Brian Whisler <brian.whisler@gmail.com>
2023-11-18 20:41:22 -0700

    footer Icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,50 @@
-import { Typography, Link } from '@mui/material';
+import { Typography, Link, Box } from '@mui/material';
+import {
+  DiReact,
+  DiMysql,
+  DiNginx,
+  DiJsBadge,
+  DiNpm,
+  DiHtml5,
+  DiCss3,
+  DiBootstrap,
+  DiDocker,
+  DiGit,
+  DiHeroku,
+  DiMongodb,
+  DiPhotoshop
+} from 'react-icons/di';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function Footer(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://spindawebdesign.com">
-        Brian Whisler
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <>
+      {' '}
+      <Box variant="body2" color="primary" align="center" {...props}>
+        <Typography fontSize={50}>
+          <DiJsBadge />
+          <DiReact />
+          <DiMysql />
+          <DiMongodb />
+          <DiHtml5 />
+          <DiCss3 />
+          <DiNpm />
+          <DiHeroku />
+          <DiNginx />
+          <DiPhotoshop />
+          <DiBootstrap />
+          <DiDocker />
+          <DiGit />
+        </Typography>
+        {'Copyright © '}
+        <Link color="inherit" href="https://spindawebdesign.com">
+          Brian Whisler
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Box>
+    </>
   );
 }
 
