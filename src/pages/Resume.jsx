a7e803438928b2463f0c3a0cf708c773923010fb
Brian Whisler <brian.whisler@gmail.com>
2023-11-18 20:15:25 -0700

    Render basic resume route

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,47 @@
+import {
+  Container,
+  List,
+  ListItem,
+  Typography
+} from '@mui/material';
+import resume from '../assets/resume.json';
+import { KeyboardArrowRight } from '@mui/icons-material';
+
+export default function Resume() {
+  let jobs = resume.jobs.map((job) => {});
+  return (
+    <>
+      <Container>
+        <Typography variant="h5" component="div" color="secondary">
+          Skills
+        </Typography>
+        <Typography variant="body2">
+          {resume.skills.map((skill, index) => (
+            <ListItem key={index} dense>
+              <KeyboardArrowRight /> {skill}
+            </ListItem>
+          ))}
+        </Typography>
+        <Typography gutterBottom variant="h5" color="secondary">
+          Work History
+        </Typography>
+        <List>
+          {resume.jobs.map((job) => (
+            <Typography key={job.company} variant="body2" component="div">
+              <Typography
+                variant="h6"
+                color="secondary"
+              >{`${job.title}: ${job.company}  ${job.timeFrame}`}</Typography>
+
+              {job.responsibilities.map((duty, index) => (
+                <ListItem key={index} dense>
+                  <KeyboardArrowRight /> {duty}
+                </ListItem>
+              ))}
+            </Typography>
+          ))}
+        </List>
+      </Container>
+    </>
+  );
+}
