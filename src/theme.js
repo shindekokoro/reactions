e281c7184ea21a9b5c5b841ac166c1e27b67ae58
Brian Whisler <brian.whisler@gmail.com>
2023-11-18 20:17:56 -0700

    update theme

--- src/theme.js
+++ src/theme.js
@@ -2,25 +2,35 @@ import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // Create a theme instance.
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     primary: {
       main: '#31343d',
       light: '#48a9a6ff',
       dark: '#31343d',
       contrastText: '#48a9a6ff',
-      lightBack: '#ededf7'
+      lightBack: '#ededf7',
+      hover: '#434753'
+    },
+    secondary: {
+      main: '#48a9a6ff',
+      light: '#ededf7',
+      dark: '#ededf7',
+      contrastText: '#ededf7'
     },
     error: {
       main: red.A400
     }
-  },
+  }
+});
+
+theme = createTheme(theme, {
   components: {
     MuiPaper: {
       styleOverrides: {
         root: {
-          backgroundColor: '#434753',
-          color: '#48a9a6ff'
+          backgroundColor: theme.palette.primary.hover,
+          color: theme.palette.primary.light
         }
       }
     },
@@ -28,8 +38,16 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           '&:hover': {
-            backgroundColor: '#ededf7'
+            backgroundColor: theme.palette.primary.lightBack
           }
+        }
+      }
+    },
+    MuiImageListItemBar: {
+      styleOverrides: {
+        title: {
+          color: theme.palette.primary.contrastText,
+          fontSizeAdjust: '0.6'
         }
       }
     }
